6feb4ade33914b067b284b9d96c32b5e02c4bd19
Ksantacr <defonix0@gmail.com>
2018-07-15 20:59:10 -0500

    Bottom navigation added

--- app/test/test.component.ts
+++ app/test/test.component.ts
@@ -3,6 +3,12 @@ import { Component, OnInit } from "@angular/core";
 import * as localStorage from 'nativescript-localstorage';
 import { Router } from "@angular/router";
 
+import { registerElement } from 'nativescript-angular';
+import { BottomBar, BottomBarItem, TITLE_STATE, SelectedIndexChangedEventData, Notification } from 'nativescript-bottombar';
+
+registerElement('BottomBar', () => BottomBar);
+
+
 @Component({
     selector: "app-test",
     moduleId: module.id,
@@ -10,6 +16,34 @@ import { Router } from "@angular/router";
 })
 export class TestComponent implements OnInit {
     message = "You have successfully authenticated. This is where you build your core application functionality.";
+
+
+    public hidden: boolean;
+    public titleState: TITLE_STATE;
+    public _bar: BottomBar;
+    public inactiveColor: string;
+    public accentColor: string;
+
+    public items: Array<BottomBarItem> = [
+        new BottomBarItem(0, "Home", "home", "black", new Notification("blue", "white", "1")),
+        new BottomBarItem(1, "Calendar", "search", "#1083BF", new Notification("green", "blue", "1")),
+        new BottomBarItem(2, "Profile", "browse", "pink", new Notification("pink", "yellow", "1")),
+        new BottomBarItem(3, "Message", "home", "green", new Notification("green", "red", "1"))
+    ];
+
+    tabLoaded(event) {
+        this._bar = <BottomBar>event.object;
+        this.hidden = false;
+        this.titleState = TITLE_STATE.SHOW_WHEN_ACTIVE;
+        this.inactiveColor = "white";
+        this.accentColor = "blue";
+    }
+    
+     tabSelected(args: SelectedIndexChangedEventData) {
+         // only triggered when a different tab is tapped
+         console.log(args.newIndex);
+         console.dir(args);
+     }
 
     constructor(private router:Router) {
         //localStorage.setItem('logeado', false);
